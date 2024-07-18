7310c646894d62b947b5f4a72d7ffcc42bbca9f2
nileshgulia1 <nileshgulia@gmail.com>
2024-07-18 18:02:32 +0530

    fix: minor fix

--- src/components/Widgets/String.jsx
+++ src/components/Widgets/String.jsx
@@ -6,7 +6,7 @@ const StringWidget = ({ val }) => {
     : Array.isArray(val)
     ? val.map((item, i) => (
         <span key={i} className="array-string-item">
-          <String val={item} key={i} />
+          <StringWidget val={item} key={i} />
         </span>
       ))
     : `${val}`;
