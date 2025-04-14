6e8424833cbe25badb6bc4ba040048422878895f
laszlocseh <csehlaszlo91@gmail.com>
2025-04-14 14:51:37 +0300

    fix eslint

--- src/components/Blocks/DemoSitesExplorer/FeatureInteraction.jsx
+++ src/components/Blocks/DemoSitesExplorer/FeatureInteraction.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { openlayers as ol } from '@eeacms/volto-openlayers-map';
 import { useMapContext } from '@eeacms/volto-openlayers-map/api';
-import { scrollToElement, zoomMapToFeatures } from './utils';
+import { zoomMapToFeatures } from './utils';
 
 export const useStyles = () => {
   const selected = React.useMemo(
